3d37436e9a0c2e7a8bd2176c7fcc0d7a469d99a5
issiy <allmylifewriter@gmail.com>
2021-10-07 23:35:48 +0900

    環境構築 PrettierとEsLintを導入する。

--- app.js
+++ app.js
@@ -2,34 +2,33 @@ const express = require('express')
 
 const app = express()
 // envファイル読み込み
-require("dotenv").config()
+require('dotenv').config()
 // formのデータ受け取り
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-const ejs = require('ejs') 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+const ejs = require('ejs')
 
 const port = 3000
-const path = require("path")
-const mysql = require("mysql")
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
+const path = require('path')
+const mysql = require('mysql')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
-const loginCheck = require("./middleware/loginCheck")
+const loginCheck = require('./middleware/loginCheck')
 
 // sequelize
-const sequelize = require("./db/db-config")
-const User = require("./models/user")
-
+const sequelize = require('./db/db-config')
+const User = require('./models/user')
 
 //　定数
-const { USER_TYPE } = require("./utils/constants")
+const { USER_TYPE } = require('./utils/constants')
 
 const sessionInfo = {
-    secret: "nodePractice",
-    cookie: { maxAge: 300000},
-    resave: false,
-    saveUninitialized: false
+  secret: 'nodePractice',
+  cookie: { maxAge: 300000 },
+  resave: false,
+  saveUninitialized: false
 }
 
 app.use(cookieParser())
@@ -37,29 +36,34 @@ app.use(cookieParser())
 app.use(session(sessionInfo))
 
 const db = mysql.createConnection({
-    host: process.env.LOCAL_DB_HOST,
-    user: process.env.LOCAL_DB_USER,
-    password: process.env.LOCAL_DB_PASSWORD,
-    database: process.env.LOCAL_DB_NAME,
-    port: process.env.LOCAL_DB_PORT
+  host: process.env.LOCAL_DB_HOST,
+  user: process.env.LOCAL_DB_USER,
+  password: process.env.LOCAL_DB_PASSWORD,
+  database: process.env.LOCAL_DB_NAME,
+  port: process.env.LOCAL_DB_PORT
 })
 
 db.connect((err) => {
-    if (err) throw err
-    console.log("mysql に接続できました")
+  if (err) throw err
+  console.log('mysql に接続できました')
 })
 
-app.use("/", require("./routing/root"))
-app.use("/home", (req, res) => {
-    res.render("home", { name: req.session.login, isLogin: req.session.login ? true : false })
+app.use('/', require('./routing/root'))
+app.use('/home', (req, res) => {
+  res.render('home', {
+    name: req.session.login,
+    isLogin: req.session.login ? true : false
+  })
 })
 
-app.use("/blog", loginCheck, (req, res) => {
-    res.render("blog", { name: req.session.login, isLogin: req.session.login ? true : false })
+app.use('/blog', loginCheck, (req, res) => {
+  res.render('blog', {
+    name: req.session.login,
+    isLogin: req.session.login ? true : false
+  })
 })
 
 //　認証系のrouting
-app.use("/auth", require("./routing/auth"))
-
+app.use('/auth', require('./routing/auth'))
 
 app.listen(port, () => console.log(`listen!! ${port}`))
--- routing/auth.js
+++ routing/auth.js
@@ -1,77 +1,75 @@
-const User = require("../models/user")
-const router = require("express").Router()
-const bcrypt = require("bcrypt")
-const loginCheck = require("../middleware/loginCheck")
-router.get("/register", (req, res) => {
-    console.log('新規登録画面の表示')
-    res.render("auth/register")
-})
+const User = require('../models/user');
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const loginCheck = require('../middleware/loginCheck');
+router.get('/register', (req, res) => {
+  console.log('新規登録画面の表示');
+  res.render('auth/register');
+});
 
-
-router.post("/register", async (req, res) => {
-    const params = req.body
-    const password = await bcrypt.hash(params.password, 10)
-    try {
-        const user = await User.findOrCreate({
-            where: { email: params.email },
-            defaults: {
-                name: params.name,
-                email: params.email,
-                type: 2,
-                password
-            }
-        })
-        if(user) {
-            res.redirect("/auth/login")
-        }
-        return res.render("auth/register")
-    } catch(err) {
-        return res.status(400).json({
-            error: {
-                message: err.message
-            }
-        })
+router.post('/register', async (req, res) => {
+  const params = req.body;
+  const password = await bcrypt.hash(params.password, 10);
+  try {
+    const user = await User.findOrCreate({
+      where: { email: params.email },
+      defaults: {
+        name: params.name,
+        email: params.email,
+        type: 2,
+        password
+      }
+    });
+    if (user) {
+      res.redirect('/auth/login');
     }
-})
+    return res.render('auth/register');
+  } catch (err) {
+    return res.status(400).json({
+      error: {
+        message: err.message
+      }
+    });
+  }
+});
 
-router.get("/login", (req, res) => {
-    console.log('ログイン画面の表示')
-    if(req.session.login){
-        res.redirect("/home")
+router.get('/login', (req, res) => {
+  console.log('ログイン画面の表示');
+  if (req.session.login) {
+    res.redirect('/home');
+  }
+  res.render('auth/login', { message: '' });
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    console.log('ログイン確認');
+    const params = req.body;
+    const user = await User.findOne({
+      where: {
+        email: params.email
+      }
+    });
+    console.log('user', user);
+    const match = await bcrypt.compare(params.password, user.password);
+    console.log('match', match);
+    if (match) {
+      console.log('マッチしました');
+      req.session.login = user.name;
+      res.render('home', { name: req.session.login, isLogin: true });
     }
-    res.render("auth/login",{ message: ""})
-})
+  } catch (err) {
+    console.log('error', err);
+    res.render('auth/login', {
+      message: 'メールアドレスもしくはパスワードが違います。'
+    });
+  }
+});
 
-router.post("/login", async (req, res) => {
-    try {
-        console.log('ログイン確認')
-        const params = req.body
-        const user = await User.findOne({
-            where: {
-                email: params.email
-            }
-        })
-        console.log("user", user)
-        const match = await bcrypt.compare(params.password, user.password)
-        console.log("match", match)
-        if(match){
-            console.log("マッチしました")
-            req.session.login = user.name
-            res.render("home", { name: req.session.login, isLogin: true })
-        }
-    }
-    catch (err) {
-        console.log("error", err)
-        res.render("auth/login", {
-            message: "メールアドレスもしくはパスワードが違います。"
-        })
-    }
-})
+router.get('/logout', (req, res) => {
+  console.log('ログアウトする');
+  req.session.login = undefined;
+  res.redirect('/');
+});
 
-router.get("/logout", (req, res) => {
-    console.log('ログアウトする')
-    req.session.login = undefined
-    res.redirect("/")
-})
-
-module.exports = router
+module.exports = router;
